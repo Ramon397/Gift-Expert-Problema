e5029b83417fe345fbda86feee4cac1ac48b1f46
Ramon Pio <ramonpioparra@gmail.com>
2024-01-02 11:57:45 -0700

    Segundo Comit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,8 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/trending?api_key=6XFnBWzDtTpPziFDrEQMJgxWZ0Tk884W&q= ${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=6XFnBWzDtTpPziFDrEQMJgxWZ0Tk884W&q= ${category}&limit=10`;
+    
+    //const url = `https://api.giphy.com/v1/gifs/trending?api_key=6XFnBWzDtTpPziFDrEQMJgxWZ0Tk884W&q= ${category}&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
